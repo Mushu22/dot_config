0e15876809e458a99f6ce91ad701490285fb73e7
YGA.PC <>
2026-05-11 00:59:28 +0200

    fix: tmux-nvim integration

--- nvim/lua/plugins/tmux.lua
+++ nvim/lua/plugins/tmux.lua
@@ -38,6 +38,35 @@ return {
 				end,
 				desc = "Resize down",
 			},
+			-- key Ctrl + hjkl defined here in order to force lazy loading when these keys are pressed
+			{
+				"<C-h>",
+				function()
+					require("tmux").move_left()
+				end,
+				desc = "Go to left pane",
+			},
+			{
+				"<C-j>",
+				function()
+					require("tmux").move_bottom()
+				end,
+				desc = "Go to bottom pane",
+			},
+			{
+				"<C-k>",
+				function()
+					require("tmux").move_top()
+				end,
+				desc = "Go to top pane",
+			},
+			{
+				"<C-l>",
+				function()
+					require("tmux").move_right()
+				end,
+				desc = "Go to right pane",
+			},
 		},
 	},
 }
